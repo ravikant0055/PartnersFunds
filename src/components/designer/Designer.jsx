f71f0fd7ef6e5c76553a8d127c6c0993b315aba7
Ravi Kant <Kumarra927@gmail.com>
2024-07-15 17:16:12 +0530

    drop area added

--- src/components/designer/Designer.jsx
+++ src/components/designer/Designer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import DesignerSidebar from './DesignerSidebar';
-//import { useDroppable } from '@dnd-kit/core';
+import { useDroppable } from '@dnd-kit/core';
+import { cn } from '../../lib/utils'
 
 const Designer = () => {
   
-  //const droppable = useDroppable();
+  const droppable = useDroppable({
+    id:"2",
+    data: {
+        type: "textfield",
+        isDesignerBtnElement : true
+    }
+  });
 
   return (
     <div className='flex w-full h-full'>
@@ -13,9 +20,22 @@ const Designer = () => {
 
        {/* page code */}
        <div className='p-4 w-full'>
-          <div className='bg-white max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center
-           justify-start flex-1 overflow-y-auto shadow-xl'>
-              <p className='text-xl text-slate-700 flex flex-grow items-center font-bold'>Drop Here</p>
+          <div 
+            ref={droppable.setNodeRef}
+            className={cn("bg-white max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto shadow-xl",
+               droppable.isOver && "ring-2 ring-primary/20"
+            )}>
+
+            {!droppable.isOver && (
+               <p className='text-xl text-slate-700 flex flex-grow items-center font-bold'>Drop Here</p>
+            )}
+            
+            {droppable.isOver && (
+               <div className='p-4 w-full'>
+                  <div className='h-[120px] rounded-md bg-primary/20'></div>
+               </div>
+            )}
+
            </div>
         </div>
     </div>

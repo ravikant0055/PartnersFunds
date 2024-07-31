90b095a5e162ae47fe962b04a7c48953049f3bf3
Hritik Jaiswal <hk4hritikjaiswal@gmail.com>
2024-07-31 16:49:42 +0530

    button properties added

--- src/components/fields/Buttons.jsx
+++ src/components/fields/Buttons.jsx
@@ -9,13 +9,28 @@ import { addprop, updateprop } from '../../store/AttributePropDataSlice';
 
 const AttributesData = {
   label:"Button",
+  color: "", // Default color
+  fontsize: "16px", // Default font size
+  fontcolor: "", // Default font color
+  height: "50px", // Default height
+  width: "200px", // Default width
 }
 
 const Buttons = ({id}) => {
   const property = useSelector((state) => state.propertiesdata.find(item => item.id === id)) || AttributesData;
   return (
     <div className='flex flex-col gap-2 w-full'>
-       <Button>{property.label}</Button>
+      <Button 
+        style={{
+          backgroundColor: property.color,
+          color: property.fontcolor,
+          fontSize: property.fontsize + "px",
+          height: property.height + "px",
+          width: property.width + "px",
+        }}
+      >
+        {property.label}
+      </Button>
     </div>
   )
 }
@@ -24,7 +39,17 @@ export function ButtonsPreview({id}){
   const property = useSelector((state) => state.propertiesdata.find(item => item.id === id)) || AttributesData;
   return (
     <div className='flex flex-col gap-2 w-full'>
-       <Button>{property.label}</Button>
+      <Button 
+        style={{
+          backgroundColor: property.color,
+          color: property.fontcolor,
+          fontSize: property.fontsize,
+          height: property.height,
+          width: property.width,
+        }}
+      >
+        {property.label}
+      </Button>
     </div>
   )
 }
@@ -45,12 +70,22 @@ export function ButtonProperties({id}) {
     defaultValues: {
       id: id,
       label: property.label,
+      color: property.color,
+      fontsize: property.fontsize,
+      fontcolor: property.fontcolor,
+      height: property.height,
+      width: property.width,
     },
   });
 
   useEffect(() => {  // Reset form values to default when the component mounts
     form.reset({
       label: property.label,
+      color: property.color,
+      fontsize: property.fontsize,
+      fontcolor: property.fontcolor,
+      height: property.height,
+      width: property.width,
     });
   }, [form, property]);
 
@@ -93,6 +128,25 @@ export function ButtonProperties({id}) {
 
         <FormField
           control={form.control}
+          name="color"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Colour</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="color"
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+<FormField
+          control={form.control}
           name="fontsize"
           render={({ field }) => (
             <FormItem>
@@ -100,6 +154,70 @@ export function ButtonProperties({id}) {
               <FormControl>
                 <Input
                   {...field}
+                  type="number"
+                  step="1"
+                  min="8"
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="fontcolor"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Font color</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="color"
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="height"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Button height (px)</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="number"
+                  step="1"
+                  min="10"
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="width"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Button width (px)</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="number"
+                  step="1"
+                  min="10"
                   onKeyDown={(e) => {
                     if (e.key === "Enter") e.currentTarget.blur();
                   }}
